95da9b18e955da1e43cf4de2595d8a5c8fe31562
Fardaan Mahdi <codingmonk501@gmail.com>
2024-04-30 12:57:22 +0530

    implemented redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import "./App.css";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentUser, setLoading, fetchUserInfo } from './lib/userStore';
 import List from "./Components/List/List";
 import Chat from "./Components/Chat/Chat";
 import Detail from "./Components/Detail/Detail";
 import Login from "./Components/Login/Login";
 import Notification from "./Components/Notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+
 
 function App() {
-  const user = true;
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const isLoading = useSelector((state) => state.user.isLoading);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    const unSub=onAuthStateChanged(auth,(user)=>{
+      if (user) {
+        dispatch(fetchUserInfo(user.uid)); // Dispatch the fetchUserInfo action
+      }
+    });
+    return ()=>{
+      unSub();
+    };
+  },[dispatch]);
+
+  console.log(currentUser);
+
+  if(isLoading) return <div className="loading p-12 text-4xl rounded-lg">Loading...</div>
   return (
     <div className="mainBody flex justify-center items-center text-white">
       <div className="w-11/12 mx-auto container rounded-lg flex">
-        {user ? (
+        {currentUser ? (
           <>
             <List />
             <Chat />
---
+++ src/lib/userStore.js
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from './firebase';
+
+export const fetchUserInfo = createAsyncThunk(
+  'user/fetchUserInfo',
+  async (uid) => {
+    if (!uid) {
+      return { currentUser: null, isLoading: false };
+    }
+
+    try {
+      const docRef = doc(db, "users", uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        return { currentUser: docSnap.data(), isLoading: false };
+      } else {
+        return { currentUser: null, isLoading: false };
+      }
+    } catch (error) {
+      console.error('Error fetching user info:', error);
+      return { currentUser: null, isLoading: false };
+    }
+  }
+);
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    currentUser: null,
+    isLoading: false
+  },
+  reducers: {
+    setCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserInfo.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUserInfo.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.currentUser = action.payload;
+    });
+  }
+});
+
+export const { setCurrentUser, setLoading } = userSlice.actions;
+
+export default userSlice.reducer;
